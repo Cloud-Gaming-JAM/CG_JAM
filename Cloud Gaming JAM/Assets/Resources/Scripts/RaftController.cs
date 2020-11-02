05bb53e793b3a6813865a8c1d2d1edc7f1d9efc3
Dorian Gélas <dorian.gelas@gmail.com>
2020-11-02 22:26:35 +0100

    [raft] [feature]  : Start RaftController
    Implementing RaftSpeedUpdater and Maximum Speed variable

---
+++ Cloud Gaming JAM/Assets/Resources/Scripts/RaftController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RaftController : MonoBehaviour
+{
+    #region PublicVariables
+    [Range(0f, 5f)] public float raftMaximumSpeed = 1f;
+    #endregion
+    #region PrivateVariables
+
+    #endregion
+    #region PrivateMethods
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    #endregion
+    #region PublicMethods
+    ///<summary>
+    ///Updates the raft instance's speed, meant to be used in a Update() loop, takes an Input Vector.
+    ///<param name="calculatedInput">Mixed input vector.</param>
+    ///</summary>
+    public void UpdateRaftSpeed(Vector2 calculatedInput)
+    {
+        Rigidbody raftRigidBody = GetComponent<Rigidbody>();
+        raftRigidBody.velocity = new Vector3(calculatedInput.x, calculatedInput.y, 0f);
+    }
+    #endregion
+}
