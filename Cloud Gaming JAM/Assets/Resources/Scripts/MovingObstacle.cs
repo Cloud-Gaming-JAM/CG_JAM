6fad0b5aec9100e9f812a500a15d4fac9b0b0f4f
Dorian Gélas <dorian.gelas@gmail.com>
2020-11-03 17:34:50 +0100

    [feat] Start of moving obstacles
    short description (wich features/bug/ect..)

---
+++ Cloud Gaming JAM/Assets/Resources/Scripts/MovingObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    [Range(0.2f, 5f)] public float obstacleSpeed = 1f;
+    Rigidbody2D obstacleRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRaftSpeed();
+    }
+
+    void UpdateRaftSpeed()
+    {
+        Vector2 raftMixedInput = new Vector2(0f, 0f);
+        obstacleRigidBody.velocity = new Vector2(-obstacleSpeed, 0f);
+    }
+}
