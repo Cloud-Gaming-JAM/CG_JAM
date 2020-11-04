e2b502c1d6fb620130030c2b27da6a38ae806b4d
Dorian Gélas <dorian.gelas@gmail.com>
2020-11-04 12:07:34 +0100

    [Feat] Fixed the default values of the AudioManager

--- Cloud Gaming JAM/Assets/Resources/Scripts/AudioManager.cs
+++ Cloud Gaming JAM/Assets/Resources/Scripts/AudioManager.cs
@@ -1,21 +1,34 @@
 ﻿using UnityEngine.Audio;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class AudioManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class Sound
+    {
+        public string clipName;
+        public AudioClip clip;
+        [HideInInspector] public AudioSource source;
+        [Range(0f, 1f)] public float volume = 0.7f;
+        [Range(.1f, 3f)] public float pitch = 1f;
+        public bool playOnAwake = false;
+        public bool loop = false;
+    }
     public GameObject MusicPrefab;
     public static float volumeGainGlobal = 0.7f;
-    public Sound[] sounds;
+    public List<Sound> sounds = new List<Sound>();
     public static AudioManager instance;
-    
 
     private void Awake()
     {
         if (instance == null)
         {
-            instance = this; 
+            instance = this;
         }
         else
         {
@@ -45,7 +58,7 @@ public class AudioManager : MonoBehaviour
         SpawnMusic();
         foreach (Sound s in sounds)
         {
-            if(s.playOnAwake)
+            if (s.playOnAwake)
             {
                 s.source.Play();
             }
@@ -54,7 +67,8 @@ public class AudioManager : MonoBehaviour
 
     public void SpawnMusic()
     {
-        if(SceneManager.GetActiveScene().buildIndex == 2){ 
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
             Instantiate(MusicPrefab);
         }
         else
@@ -73,7 +87,7 @@ public class AudioManager : MonoBehaviour
     }
     public void Play(string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = sounds.Find(sound => sound.clipName == name);
         if (s == null)
         {
             Debug.LogWarning("Sound " + name + " not found!");
@@ -85,7 +99,7 @@ public class AudioManager : MonoBehaviour
 
     public void Stop(string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = sounds.Find(sound => sound.clipName == name);
         if (s == null)
         {
             Debug.LogWarning("Sound " + name + " not found!");
@@ -99,7 +113,7 @@ public class AudioManager : MonoBehaviour
 
     public void PitchChange(string name, float _pitch)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = sounds.Find(sound => sound.clipName == name);
         if (s == null)
         {
             Debug.LogWarning("Sound " + name + " not found!");
@@ -111,7 +125,7 @@ public class AudioManager : MonoBehaviour
 
     public void VolumeChange(string name, float _volume)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = sounds.Find(sound => sound.clipName == name);
         if (s == null)
         {
             Debug.LogWarning("Sound " + name + " not found!");
@@ -123,17 +137,5 @@ public class AudioManager : MonoBehaviour
     #endregion
 }
 
-[System.Serializable]
-public class Sound
-{
-    public string name;
 
-    public AudioClip clip;
-    
-    [HideInInspector] public AudioSource source;
-    [Range(0f, 1f)] public float volume = 0.7f;
-    [Range(.1f, 3f)] public float pitch = 1f;
-    public bool playOnAwake;
-    public bool loop;
-}
 
