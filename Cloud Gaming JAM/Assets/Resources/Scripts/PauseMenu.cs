f025519c2148340b734c37537eda7c9824868955
Dorian Gélas <dorian.gelas@gmail.com>
2020-11-06 23:19:10 +0100

    [feat] endscreen + pause screen

---
+++ Cloud Gaming JAM/Assets/Resources/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Time.timeScale = 0f;
+    }
+
+    void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && !gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
