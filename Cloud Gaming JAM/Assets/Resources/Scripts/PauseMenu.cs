2e9a53c014eb06c1470dda971e125d976be9ae66
Dorian Gélas <dorian.gelas@gmail.com>
2020-11-08 12:50:27 +0100

    [feat/asset/refactor] Pause and End screen + Video BG in menu

--- Cloud Gaming JAM/Assets/Resources/Scripts/PauseMenu.cs
+++ Cloud Gaming JAM/Assets/Resources/Scripts/PauseMenu.cs
@@ -4,18 +4,21 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static PauseMenu instance;
-    void Init()
+    void Awake()
     {
         instance = this;
+        gameObject.SetActive(false);
     }
     void OnEnable()
     {
-        Time.timeScale = 0f;
+        GameManager.instance.SetGameState(GameState.pause);
+        Time.fixedDeltaTime = 0f;
     }
 
     void OnDisable()
     {
-        Time.timeScale = 1f;
+        GameManager.instance.SetGameState(GameState.inGame);
+        Time.fixedDeltaTime = 0.02f;
     }
 
     void Update()
@@ -26,6 +29,7 @@ public class PauseMenu : MonoBehaviour
 
     public void DisplayPauseMenu()
     {
+        Debug.Log("AttemptToDisplayPauseMenu");
         if (gameObject.activeSelf)
         {
             gameObject.SetActive(false);
@@ -41,7 +45,7 @@ public class PauseMenu : MonoBehaviour
     }
     public void BackToMenu()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(0);
     }
 
 }
