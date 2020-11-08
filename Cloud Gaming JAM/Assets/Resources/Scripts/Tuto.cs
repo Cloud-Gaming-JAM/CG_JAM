b0b80019d240ca06fc2e4d8c5b0ec7ff690d9c56
Ulysse <ulysse.raillon@hotmail.fr>
2020-11-08 13:54:34 +0100

    [feat] tuto script

---
+++ Cloud Gaming JAM/Assets/Resources/Scripts/Tuto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuto : MonoBehaviour
+{
+    public float[] raftsPos = new float[LevelManager.instance.nbrRaftInGame];
+
+    private bool over;
+    void Update()
+    {
+        if(!over)
+            UpdateRaftPos();
+    }
+
+    void UpdateRaftPos()
+    {
+        int raftCrossed = 0;
+        for (int i = 0; i < raftsPos.Length; i++)
+        {
+            raftsPos[i] = LevelManager.instance.rafts[i].transform.position.x;
+            if (raftsPos[i] > transform.position.x)
+                raftCrossed++;
+        }
+        if(raftCrossed == raftsPos.Length)
+            DisableTuto();
+    }
+
+    void DisableTuto()
+    {
+        over = true;
+        Debug.Log("Disable tuto");
+        //this.gameObject.SetActive(false);
+    }
+}
