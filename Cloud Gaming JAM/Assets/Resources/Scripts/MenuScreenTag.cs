7b156c91718cbe30b30cc7fd816f7978c6754f8e
Dorian Gélas <dorian.gelas@gmail.com>
2020-11-08 14:21:10 +0100

    [feat] Character selection is reset when exiting menu

--- Cloud Gaming JAM/Assets/Resources/Scripts/MenuScreenTag.cs
+++ Cloud Gaming JAM/Assets/Resources/Scripts/MenuScreenTag.cs
@@ -5,4 +5,24 @@ using UnityEngine;
 public class MenuScreenTag : MonoBehaviour
 {
     public MenuController.MenuScreen menuScreenType = 0;
+
+    void OnDisable()
+    {
+        if (menuScreenType == MenuController.MenuScreen.CharacterSelection)
+        {
+            foreach (PlayerController player in GameManager.instance.players)
+            {
+                player.playerId = 0;
+                player.teamId = 0;
+            }
+            foreach (RaftController raft in LevelManager.instance.rafts)
+            {
+                raft.playersOnRaft.Clear();
+            }
+            MenuController.instance.SetActiveLeavePlayerImage(1);
+            MenuController.instance.SetActiveLeavePlayerImage(2);
+            MenuController.instance.SetActiveLeavePlayerImage(3);
+            MenuController.instance.SetActiveLeavePlayerImage(4);
+        }
+    }
 }
